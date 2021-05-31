d46374c5a13a71f4d0d5c4998d661cd9d623a97d
Benjamin Roche <benjamin.roche4@gmail.com>
2021-05-31 22:32:44 +0200

    Fixtures users

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Customer;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,14 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for($i = 1; $i <=50; $i++){
+            $user = new User();
+            $user->setFirstname('John')
+                ->setLastname('Doe')
+                ->setEmail('johndoe@gmail.com')
+                ->setCreatedAt(new \DateTime())
+                ->setCustomerId($manager->find(Customer::class, 40));
+        }
 
         $manager->flush();
     }
